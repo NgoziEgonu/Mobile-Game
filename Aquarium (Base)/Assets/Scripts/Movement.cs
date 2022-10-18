fc942b2dd358666b38e278ad88d54299b65b0509
NgoziEgonu <71980104+NgoziEgonu@users.noreply.github.com>
2022-10-18 22:19:05 +0100

    Flipping Mechanic being worked on

--- Aquarium (Base)/Assets/Scripts/Movement.cs
+++ Aquarium (Base)/Assets/Scripts/Movement.cs
@@ -9,13 +9,12 @@ public class Movement : MonoBehaviour
     public GameObject dolphin;
     public Rigidbody dolphin_rb;
 
+    ScoreCounter scoreCounter;
+
+    float flip_Rate;
     public float rot_Rate;
     public float speed;
     //public TextMeshProUGUI spins_Counter;
-
-    ScoreCounter scoreCounter;
-
-    float score;
 
     float tilt_Move;
 
@@ -26,8 +25,9 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        flip_Rate = Mathf.Rad2Deg * (100.0f) * Time.deltaTime;
         dolphin_rb = GetComponent<Rigidbody>();
-        scoreCounter = FindObjectOfType<ScoreCounter>();
+        scoreCounter = FindObjectOfType<ScoreCounter>(); // Reference https://stackoverflow.com/questions/34436458/unity-c-how-to-call-a-function-from-another-script-to-start-animation
     }
 
     // Update is called once per frame
@@ -38,23 +38,20 @@ public class Movement : MonoBehaviour
 
         TouchControls();
 
-
-        //if ((Mathf.Rad2Deg * dolphin.transform.rotation.y) == 100.0f)
+        //Flip Mechanic Not working properly yet
+        //if (Input.touchCount > 0)
         //{
-        //    spins++;
-        //    Debug.Log(spins);
-
-        //    spins_Counter.text = "" + spins;
+        //    Touch touch = Input.GetTouch(0);
+        //    if (touch.tapCount == 2)
+        //    {
+        //        dolphin.transform.Rotate(new Vector3(0, 0, flip_Rate ));
+        //    }
         //}
-
-        //Debug.Log((Mathf.Rad2Deg * dolphin.transform.rotation.y));
 
     }
 
     void TouchControls()
     {
-        
-        
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
@@ -80,7 +77,7 @@ public class Movement : MonoBehaviour
 
                     dolphin.transform.Rotate(new Vector3(0, rot_Rate * Time.deltaTime, 0));
                 }
-                
+
             }
 
             if (touch.phase == TouchPhase.Ended)
